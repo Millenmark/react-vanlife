1810e4bdedf1bb3dba8375fc8e613423581366a1
Millenmark <millenmark.iv@gmail.com>
2023-05-03 19:54:40 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from "react-router-dom"
+import { DefaultLayout } from "./layouts"
 
 function App() {
 
   return (
     <>
-      <h1>Hello world</h1>
+      <Routes>
+        <Route path="/" element={<DefaultLayout/>}>
+          <Route path="/" element={<h1>Hello World</h1>}/>
+          <Route path="/about" element={<h1>This is an about page</h1>}/>
+        </Route>
+      </Routes>
     </>
   )
 }
